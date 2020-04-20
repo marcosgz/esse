4a52b7c74909d787973bb19b172fa4489e2818c3
Marcos G. Zimmermann <mgzmaster@gmail.com>
2020-04-20 14:20:24 -0300

    Create some helper classes for primitive objects like String, Integer, etc

---
+++ lib/esse/primitives/hstring.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+module Esse
+  # The idea here is to add useful methods to the ruby core objects without
+  # monkey patching.
+  # And on this state and not thinking about to add ActiveSupport dependency
+  class Hstring
+    extend Forwardable
+
+    def_delegators :@value, :==, :eq, :to_s, :inspect, :sub, :capitalize
+
+    def self.def_conventional(bang_method, conv_method = nil)
+      conv_method ||= bang_method.to_s.sub(/[!?]*$/, '')
+      if public_instance_methods.include?(conv_method)
+        msg = format(
+          'Equivalent %<conv>p already defined for the bang method %<bang>p',
+          conv: conv_method.to_s,
+          bang: bang_method.to_s,
+        )
+        raise(SyntaxError, msg)
+      end
+
+      unless public_instance_methods.include?(bang_method)
+        msg = format(
+          'Undefined method %<bang>p for %<klass>p',
+          bang: bang_method.to_s,
+          klass: self,
+        )
+        raise(SyntaxError, msg)
+      end
+
+      define_method(conv_method) do |*args|
+        self.class.new(self).public_send(bang_method, *args)
+      end
+    end
+
+    def initialize(value)
+      @value = value.to_s
+    end
+
+    def camelize!
+      @value = @value.split(/(?=[_A-Z])/).map { |str| str.tr('_', '').capitalize }.join
+      self
+    end
+    def_conventional :camelize!, :camelize
+
+    def demodulize!
+      @value = @value.split('::').last
+      self
+    end
+    def_conventional :demodulize!
+
+    def modulize!
+      @value = @value.split(/\:\:|\\|\//).map { |part| self.class.new(part).camelize.to_s }.join('::')
+      self
+    end
+    def_conventional :modulize!
+
+    def underscore!
+      @value = @value
+        .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
+        .gsub(/([a-z\d])([A-Z])/, '\1_\2')
+        .tr('-', '_')
+        .gsub(/\s/, '_')
+        .gsub(/__+/, '_')
+        .downcase
+      self
+    end
+    def_conventional :underscore!
+  end
+end
---
+++ spec/esse/primitives/hstring_spec.rb
@@ -0,0 +1,101 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Esse::Hstring do
+  let(:model) { described_class.new(arg) }
+
+  describe '.underscore' do
+    subject { model.underscore }
+
+    context 'with capitalized string' do
+      let(:arg) { 'User' }
+
+      it { is_expected.to eq('user') }
+    end
+
+    context 'with camelized string' do
+      let(:arg) { 'UserName' }
+
+      it { is_expected.to eq('user_name') }
+    end
+
+    context 'with camelized string' do
+      let(:arg) { 'UserName' }
+
+      it { is_expected.to eq('user_name') }
+    end
+
+    context 'with parameterized string' do
+      let(:arg) { 'foo-bar' }
+
+      it { is_expected.to eq('foo_bar') }
+    end
+  end
+
+  describe '.demodulize' do
+    subject { model.demodulize }
+
+    context 'with single class' do
+      let(:arg) { 'User' }
+
+      it { is_expected.to eq('User') }
+    end
+
+    context 'with only one level modulized string' do
+      let(:arg) { '::User' }
+
+      it { is_expected.to eq('User') }
+    end
+
+    context 'with multiple modulized string' do
+      let(:arg) { '::Foo::Bar' }
+
+      it { is_expected.to eq('Bar') }
+    end
+  end
+
+  describe '.modulize' do
+    subject { model.modulize }
+
+    context 'delimited by backslashes' do
+      let(:arg) { 'foo\bar'}
+
+      it { is_expected.to eq('Foo::Bar') }
+    end
+
+    context 'delimited by forward slashes' do
+      let(:arg) { 'foo/bar'}
+
+      it { is_expected.to eq('Foo::Bar') }
+    end
+
+    context 'modulized like argument' do
+      let(:arg) { 'Foo::bar_baz'}
+
+      it { is_expected.to eq('Foo::BarBaz') }
+    end
+  end
+
+  describe '.camelize' do
+    subject { model.camelize }
+
+    context 'with a symbol' do
+      let(:arg) { :user }
+
+      it { is_expected.to eq('User') }
+    end
+
+    context 'with snake string' do
+      let(:arg) { 'user_index' }
+
+      it { is_expected.to eq('UserIndex') }
+    end
+
+    context 'with partial upercased string' do
+      let(:arg) { 'userIndex' }
+
+      it { is_expected.to eq('UserIndex') }
+    end
+  end
+end
