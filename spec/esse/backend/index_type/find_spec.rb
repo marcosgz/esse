78681ccff782542eb840a5ce5e015b4128f84857
Marcos G. Zimmermann <mgzmaster@gmail.com>
2020-05-10 06:35:10 -0300

    move the index type backend #find! to the find_spec

--- spec/esse/backend/index_type/find_spec.rb
+++ spec/esse/backend/index_type/find_spec.rb
@@ -6,6 +6,31 @@ require 'support/shared_contexts/geos_index_definition'
 RSpec.describe Esse::Backend::Index do
   include_context 'geos index definition'
 
+  describe '.find!' do
+    let(:data) { { 'name' => 'Illinois', 'pk' => 1 } }
+
+    specify do
+      es_client do
+        expect { GeosIndex::State.backend.find!(id: data['pk']) }.to raise_error(
+          Elasticsearch::Transport::Transport::Errors::NotFound,
+        )
+      end
+    end
+
+    specify do
+      es_client do
+        expect(GeosIndex::State.backend.index(id: data['pk'], body: data)['created']).to eq(true)
+        response = GeosIndex::State.backend.find!(id: data['pk'])
+        expect(response['_id']).to eq('1')
+        expect(response['_source']).to eq(data)
+        expect(response['_type']).to eq('state')
+        expect { GeosIndex::County.backend.find!(id: data['pk']) }.to raise_error(
+          Elasticsearch::Transport::Transport::Errors::NotFound,
+        )
+      end
+    end
+  end
+
   describe '.find' do
     let(:data) { { 'name' => 'Illinois', 'pk' => 1 } }
   
