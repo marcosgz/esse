7d469ed98e8650eca790d2281ff5b006aff2e8dc
Marcos G. Zimmermann <mgzmaster@gmail.com>
2024-08-02 14:37:54 -0300

    feat: fix specs for es that use type

--- spec/esse/integrations/elasticsearch-6/transport/documents_update_by_query_spec.rb
+++ spec/esse/integrations/elasticsearch-6/transport/documents_update_by_query_spec.rb
@@ -4,5 +4,5 @@ require 'spec_helper'
 require 'support/shared_examples/transport_update_by_query'
 
 stack_describe 'elasticsearch', '6.x', Esse::Transport, '#update_by_query' do
-  include_examples 'transport#update_by_query'
+  include_examples 'transport#update_by_query', doc_type: true
 end
--- spec/support/shared_examples/transport_update_by_query.rb
+++ spec/support/shared_examples/transport_update_by_query.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
-RSpec.shared_examples 'transport#update_by_query' do
+RSpec.shared_examples 'transport#update_by_query'  do |doc_type: false|
+  let(:params) do
+    doc_type ? { type: 'geo' } : {}
+  end
   let(:body) do
     {
       settings: {
@@ -18,7 +21,7 @@ RSpec.shared_examples 'transport#update_by_query' do
       expect(client).not_to receive(:perform_request)
       cluster.readonly = true
       expect {
-        cluster.api.update_by_query(index: "#{cluster.index_prefix}_redonly", body: { script: { source: 'ctx._source.title = "foo"' } }, q: '*')
+        cluster.api.update_by_query(**params, index: "#{cluster.index_prefix}_redonly", body: { script: { source: 'ctx._source.title = "foo"' } }, q: '*')
       }.to raise_error(Esse::Transport::ReadonlyClusterError)
     end
   end
@@ -26,7 +29,7 @@ RSpec.shared_examples 'transport#update_by_query' do
   it 'raises an #<Esse::Transport::NotFoundError exception when the source index does not exist' do
     es_client do |_client, _conf, cluster|
       expect {
-        cluster.api.update_by_query(index: "#{cluster.index_prefix}_non_existent_index", body: { script: { source: 'ctx._source.title = "foo"' }, query: { match_all: {} } })
+        cluster.api.update_by_query(**params, index: "#{cluster.index_prefix}_non_existent_index", body: { script: { source: 'ctx._source.title = "foo"' }, query: { match_all: {} } })
       }.to raise_error(Esse::Transport::NotFoundError)
     end
   end
@@ -36,11 +39,11 @@ RSpec.shared_examples 'transport#update_by_query' do
       es_client do |client, _conf, cluster|
         index_name = "#{cluster.index_prefix}_update_by_query"
         cluster.api.create_index(index: index_name, body: body)
-        cluster.api.index(index: index_name, id: 1, body: { title: 'old title' }, refresh: true)
+        cluster.api.index(**params, index: index_name, id: 1, body: { title: 'old title' }, refresh: true)
 
         resp = nil
         expect {
-          resp = cluster.api.update_by_query(index: index_name, body: { script: { source: 'ctx._source.title = "new title"' }, query: { match_all: {} } })
+          resp = cluster.api.update_by_query(**params, index: index_name, body: { script: { source: 'ctx._source.title = "new title"' }, query: { match_all: {} } })
         }.not_to raise_error
         expect(resp['total']).to eq(1)
         expect(resp['updated']).to eq(1)
